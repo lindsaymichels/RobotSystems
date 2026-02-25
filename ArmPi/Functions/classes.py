034957de454e8e440667132fc2629a5eac953612
lindsaymichels <lindsay.a.michels@gmail.com>
2026-02-25 11:16:08 -0800

    motion

--- ArmPi/Functions/classes.py
+++ ArmPi/Functions/classes.py
@@ -163,7 +163,7 @@ class Motion:
 
                         if not self.is_running:
                             continue
-                        self.ak.setPitchRangeMoving((world_X, world_Y, 1), -90, -90, 0, 1000)
+                        self.ak.setPitchRangeMoving((world_X, world_Y, 0.6), -90, -90, 0, 1000)
                         time.sleep(2)
 
                         if not self.is_running:
